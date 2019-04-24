57a42b3c48894b298898a306d195f38d61790c04
Marissa <marissa.verdonck@hva.nl>
2019-04-24 07:56:47 +0200

    comments toegevoegd

--- Week 1/FizzBuzz.js
+++ Week 1/FizzBuzz.js
@@ -1,5 +1,6 @@
 // FizzBuzz
 for (let counter = 1; counter <= 100; counter++) {
+//Er wordt een getallenreeks gemaakt, van 1 tot 100.
 
     let tekst1 = "Fizz";
     let tekst2 = "Buzz";
@@ -7,11 +8,13 @@ for (let counter = 1; counter <= 100; counter++) {
 
     if (counter % 3 == 0 && counter % 5 == 0) {
         console.log(tekst3)
+        // Als in de getallenreeks een getal in de tafel van 3 of 5 voorkomt, verschijnt er een string.
     } else if (counter % 3 == 0) {
         console.log(tekst1)
     } else if (counter % 5 == 0) {
         console.log(tekst2)
     }
-
-    console.log(counter)
+    else{console.log(counter)}
 }
+
+
--- Week 1/Loopingatriangle.js
+++ Week 1/Loopingatriangle.js
@@ -1,3 +1,4 @@
 // Looping a triangle
 for (let hekje = "#"; hekje.length <= 6; hekje += "#")
     console.log(hekje);
+/* Er wordt 6 keer een hekje geplaatst. Elke ronde komt er een hekje bij. De eerste keer is de waarde van het hekje #, de tweede keer ##, enz.*/
