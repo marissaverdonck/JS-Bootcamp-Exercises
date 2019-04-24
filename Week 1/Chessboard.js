2956efc0576ae67a3a5d3fc27898d25227ecf37a
Marissa <marissa.verdonck@hva.nl>
2019-04-24 08:07:55 +0200

    Comments toegevoegd

--- Week 1/Chessboard.js
+++ Week 1/Chessboard.js
@@ -1,12 +1,18 @@
 // Chessboard
-for (let counter = 0; counter <= 7; counter++) {
+let i = 7;
+
+for (let counter = 0; counter < i; counter++) {
+// de waarde van i bepaald hoeveel rijen van het chessboard worden gemaakt
 
     let hekjes = "# # # #";
     let spatie = " ";
 
-    if (counter % 2 == 0) {
+    if (counter % 2) {
         console.log(hekjes)
+        // Wanneer de counter een even getal is zal de rij hekjes worden getoond
     } else {
         console.log(spatie + hekjes)
+         // Wanneer de counter een oneven getal is zal de rij hekjes worden getoond met een spatie ervoor
     }
 }
+
