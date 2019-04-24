4be1c5a9b0fe8a56d95f9d0ec0ffaa04d31f13b6
Marissa <marissa.verdonck@hva.nl>
2019-04-24 08:18:04 +0200

    comments toegevoegd

--- Week 2/Minimum.js
+++ Week 2/Minimum.js
@@ -1,9 +1,11 @@
 // Minimum
 function min(x, y) {
-  if (x < y) return x;
-  else return y;
+return (Math.min(x,y));
+//Math.min geeft de laagste waarde terug
 }
 
 console.log(min(0, 10));
 console.log(min(0, -10));
+
+
 
