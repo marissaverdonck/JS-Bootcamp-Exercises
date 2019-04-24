d5c29ec92c100d75230616730e89933bb197526b
Marissa <marissa.verdonck@hva.nl>
2019-04-24 08:36:04 +0200

    Comments toegevoegd

--- Week 2/The sum of a range.js
+++ Week 2/The sum of a range.js
@@ -1,10 +1,12 @@
 // The sum of a range
 function range(start, end) {
     let getallenReeks = [];
+    //Hier komt de getallenreeks in de staan vanuit de loop
 
     if (start < end) {
         for (let i = 0; i < end; i++)
             getallenReeks.push(start + i);
+        //Zolang start kleiner is dan end, zal er in de loop steeds +1 bij start bijkomen. dit wordt in de getallenrReeks array gepusht.
     }
     return getallenReeks;
 }
@@ -12,8 +14,10 @@ function range(start, end) {
 function sum(getallenReeks) {
     let optelling = 0;
     for (let i = 0; i < 10; i++) {
+        //De getallenreeks van functie range wordt doorlopen met i.
 
         optelling += getallenReeks[i];
+        //Elke waarde uit de array (i) wordt bij optelling geteld
     }
     return optelling;
 }
